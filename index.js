8127b6362a62685ea7c0231d7a8536e0479fe6a3
Erinc Emer <erincemer@gmail.com>
2019-07-14 11:45:27 -0700

    handlSubmit added

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Document</title>
-</head>
-<body>
-    <main>
-        <div class='main-header'>
-                <img src="https://image.ibb.co/e6vOFQ/wikipedia.png" alt="Wikipedia Logo"/>
-                <h1>MiniPedia</h1>
-                <img src="https://image.ibb.co/e6vOFQ/wikipedia.png" alt="Wikipedia Logo"/>
-        </div>
-        <header class="searchForm-container">
-            <form class="searchForm">
-                <input type="search" class="searchForm-input"/>
-                    <button type="submit" class="icon searchIcon">
-                        <img src="https://image.ibb.co/cpG8zk/search.png" alt="Magnifying Glass Icon"/>
-                    </button>
-            </form>
-        </header>
-            <section class="searchResults"></section>
-    </main>
-</body>
-</html>
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = document.querySelector('.searchForm-input').value;
+    const searchQuery = input.trim();
+    if (searchQuery === "") {
+        emptyEntry();
+    }
+    fetchResults(searchQuery);
+    document.querySelector('.searchForm').reset();
+
+}
