411867b994575dc6bb78ba2c7f74f8d5d8ce0094
Erinc Emer <erincemer@gmail.com>
2019-07-18 17:38:06 -0700

    fetchResults added

--- index.js
+++ index.js
@@ -19,3 +19,19 @@ function emptyEntry() {
         </div>`
     );
 }
+
+function fetchResults(searchQuery) {
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            const results = data.query.search;
+            const searchInfo = data.query.searchinfo;
+            if (results.length === 0) {
+                invalidEntryResult(searchInfo, searchQuery)
+            } else {
+                displayResults(results, searchQuery);
+            }
+        })
+        .catch(() => console.log('An error occurred'));
+}
